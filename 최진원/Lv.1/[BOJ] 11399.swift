825974405a3447ac162032915ad3813f24e45ee5
Jinwon Choi <jjwls97@catholic.ac.kr>
2025-03-07 18:30:40 +0900

    [BOJ]_11399_1931_최진원

---
+++ 최진원/Lv.1/[BOJ] 11399.swift
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/11399
+
+var n = Int(readLine()!)!
+var p = readLine()!.split(separator: " ").compactMap { Int($0) }
+p.sort { $0 < $1 }
+var answer = 0
+p.forEach {
+    answer += $0 * n
+    n -= 1
+}
+print(answer)
